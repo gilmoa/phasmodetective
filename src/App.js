0df0faaf9d6c0e13fb3ee147e6a70aa6ffee6e03
Andrea Gilmozzi <agilmozzi@gmail.com>
2020-10-29 18:46:02 +0100

    adds firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import React from "react";
 import Layout from "./components/Layout";
 import Main from "./components/Main";
 
+import firebase from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebase.initializeApp({
+  apiKey: "AIzaSyA8xn5bp-1YcSYiUwesEEeu5AN9m3XUVZU",
+  authDomain: "phasmodetective.firebaseapp.com",
+  databaseURL: "https://phasmodetective.firebaseio.com",
+  projectId: "phasmodetective",
+  storageBucket: "phasmodetective.appspot.com",
+  messagingSenderId: "518405251228",
+  appId: "1:518405251228:web:fb935a4a3401dd04774d59",
+  measurementId: "G-MEYR4X3JHC",
+});
+
 const App = () => {
   return (
     <Layout>
